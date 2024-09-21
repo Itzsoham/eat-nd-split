aa69146d1218c248e497ce611d5d08b030b8ff45
kai <137367860+Itzsoham@users.noreply.github.com>
2024-09-21 22:19:19 +0530

    Final Push

--- src/App.js
+++ src/App.js
@@ -145,13 +145,13 @@ function FormAddFriend({ onAddFriend }) {
 
   return (
     <form className="form-add-friend" onSubmit={handleSubmit}>
-      <label>🖼️ Friend Name</label>
+      <label>Friend Name</label>
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
       ></input>
-      <label>🖼️ Image Url</label>
+      <label>Image Url</label>
       <input
         type="text"
         value={image}
